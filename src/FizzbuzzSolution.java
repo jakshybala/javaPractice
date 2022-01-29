55c647bd3c354ffdee33ee44df73ad09fb62c150
jakshybala <maximuspro.kg@gmail.com>
2022-01-29 13:47:45 +0600

    echo "# javaPractice"

---
+++ src/FizzbuzzSolution.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class FizzbuzzSolution {
+    public static void main(String[] args) {
+//        int num = returnInt(4);
+//        System.out.println(num *2);
+
+        System.out.println(1<5? "True": "False");
+
+        int i =0;
+        do {
+            System.out.println();
+        }
+        while (i<10);
+
+
+
+
+
+
+
+    }
+    static int returnInt (int n){
+        Scanner enterLengh = new Scanner(System.in);
+        System.out.println("Введите цифру: ");
+        return enterLengh.nextInt();
+
+    }
+}
