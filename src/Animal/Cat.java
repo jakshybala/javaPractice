a4e84b608000691e5cf4e37eefd6c1596539ce27
jakshybala <maximuspro.kg@gmail.com>
2022-02-06 01:56:25 +0600

    Animal

---
+++ src/Animal/Cat.java
@@ -0,0 +1,47 @@
+package Animal;
+
+import java.util.Scanner;
+
+public class Cat {
+    private String cName;
+    private String cKind;
+    private int cAge;
+
+    public void checkCatData() {
+        Scanner enterData = new Scanner(System.in);
+        while (cAge <= 0) {
+            System.out.println("Retype: Cats age " + cAge + "\nMust be int data!!!");
+            cAge = enterData.nextInt();
+        }
+    }
+    public void getCatInfo() {
+            System.out.println("Cats name: " + cName + "\nCats Kind: " + cKind + "\nCats age: " + cAge);
+
+
+    }
+
+    public String getcName() {
+        return cName;
+    }
+
+    public void setcName(String cName) {
+        this.cName = cName;
+    }
+
+    public String getcKind() {
+        return cKind;
+    }
+
+    public void setcKind(String cKind) {
+        this.cKind = cKind;
+    }
+
+    public int getcAge() {
+        return cAge;
+    }
+
+    public void setcAge(int cAge) {
+        this.cAge = cAge;
+    }
+}
+
---
+++ src/Animal/Parrot.java
@@ -0,0 +1,31 @@
+package Animal;
+
+public class Parrot {
+    private String pName;
+    private String pKind;
+    private int pAge;
+
+    public String getpName() {
+        return pName;
+    }
+
+    public void setpName(String pName) {
+        this.pName = pName;
+    }
+
+    public String getpKind() {
+        return pKind;
+    }
+
+    public void setpKind(String pKind) {
+        this.pKind = pKind;
+    }
+
+    public int getpAge() {
+        return pAge;
+    }
+
+    public void setpAge(int pAge) {
+        this.pAge = pAge;
+    }
+}
